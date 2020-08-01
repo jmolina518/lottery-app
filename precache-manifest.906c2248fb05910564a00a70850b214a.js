984037fe78cce7f6b74cd06a6c38c4f4f151cb5c
jmolina518 <jmolina518@gmail.com>
2020-08-01 11:55:53 -0500

    Updates

---
+++ precache-manifest.906c2248fb05910564a00a70850b214a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07128e430474afdfc84ec64c1009eda0",
+    "url": "/lottery-app/index.html"
+  },
+  {
+    "revision": "d94d33d580990617737d",
+    "url": "/lottery-app/static/css/main.9e453217.chunk.css"
+  },
+  {
+    "revision": "89b1f2e9078edf257379",
+    "url": "/lottery-app/static/js/2.0c2e5682.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lottery-app/static/js/2.0c2e5682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94d33d580990617737d",
+    "url": "/lottery-app/static/js/main.8e0c8ddf.chunk.js"
+  },
+  {
+    "revision": "9baab0a3154b258a195f",
+    "url": "/lottery-app/static/js/runtime-main.1fed992a.js"
+  }
+]);
